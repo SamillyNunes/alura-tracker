14c92c7172e5674c9d81478663f466acb8e24031
Samilly <samillynunes19@gmail.com>
2024-12-31 17:08:18 -0300

    feat: side bar component added and chore: prettier removed

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
   
-  
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <SideBar />
+    </div>
+
+    
+
+  </main>
 
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import SideBar from "./components/SideBar.vue";
 
 export default defineComponent({
   name: "App",
+  components: { SideBar }
 });
 </script>
 
---
+++ src/components/SideBar.vue
@@ -0,0 +1,40 @@
+<template>
+
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt="Logo da aplicação" />
+        </h1>
+    </header>
+
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'SideBar',
+});
+
+</script>
+
+<style lang="css" scoped>
+
+header{
+    background: #0d3b66;
+    width: 100%;
+    height: 100vh;
+    padding-top: 1rem;
+
+    display: flex;
+    justify-content: center;
+}
+
+@media only screen and (max-width: 768px){
+    header{
+        padding: 2.5rem;
+        height: auto;
+    }    
+}
+
+</style>
