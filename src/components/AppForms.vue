3d8cbdceb2552471e32a81944712c09f52929f38
Samilly <samillynunes19@gmail.com>
2024-12-31 17:40:43 -0300

    feat: starting, presenting and formatting timer

--- src/components/AppForms.vue
+++ src/components/AppForms.vue
@@ -13,17 +13,17 @@
         class="column is-flex is-align-items-center is-justify-content-space-between"
       >
         <section>
-          <strong>00:00:00</strong>
+          <strong>{{ timeFormated }}</strong>
         </section>
 
-        <button class="button">
+        <button class="button" @click="startTimer">
           <span class="icon">
             <i class="fas fa-play"></i>
           </span>
           <span>play</span>
         </button>
 
-        <button class="button">
+        <button class="button" @click="stopTimer">
           <span class="icon">
             <i class="fas fa-stop"></i>
           </span>
@@ -39,5 +39,29 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "AppForms",
+  data (){
+    return {
+      timeInSeconds: 0,
+    }
+  },
+  computed: {
+    timeFormated(): string {
+      // Primeiro vai passar os segundos convertido para milissegundos, para que o Date absorva a informacao de tempo
+      // Depois vai passar para o formato: 2024-12-08T15:45:00 , e por isso pegamos em seguida a partir da posicao 11, ate o caractere 19
+      return new Date(
+        this.timeInSeconds*1000,
+      ).toISOString().substring(11,19);
+    }
+  },
+  methods: {
+    startTimer(){
+      setInterval(() => {
+        this.timeInSeconds+=1;
+      }, 1000);
+    },
+    stopTimer(){
+      console.log('Finalizando...');
+    }
+  },
 });
 </script>
