b871dac823f65da8be75bb17136bc409732d2725
Samilly <samillynunes19@gmail.com>
2025-01-01 11:14:57 -0300

    feat: custom task component created

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,13 @@
 
     <div class="column is-three-quarter">
       <AppForms />
+
+      <div class="lista">
+        <CustomTask />
+        <CustomTask />
+        <CustomTask />
+      </div>
+
     </div>
 
     
@@ -19,13 +26,18 @@
 import { defineComponent } from "vue";
 import SideBar from "./components/SideBar.vue";
 import AppForms from "./components/AppForms.vue";
+import CustomTask from "./components/CustomTask.vue";
 
 export default defineComponent({
   name: "App",
-  components: { SideBar, AppForms }
+  components: { SideBar, AppForms, CustomTask }
 });
 </script>
 
-<style>
+<style scoped>
+
+.lista{
+  padding: 1.25rem;
+}
 
 </style>
---
+++ src/components/CustomTask.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <div class="box has-text-weight-bold">
+        <div class="columns">
+            <div class="column is-7">
+                Descrição da tarefa
+            </div>
+
+            <div class="column">
+                <TimerDisplay :timeInSeconds="15" />
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TimerDisplay from './TimerDisplay.vue';
+
+
+export default defineComponent({
+    name: 'CustomTask',
+    components: { TimerDisplay }
+});
+
+</script>
