24c3155b493969dc610bf4075b032e2bbb93eda3
Samilly <samillynunes19@gmail.com>
2025-01-02 11:17:39 -0300

    feat: presenting projects in Projects view

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,14 +1,80 @@
 <template>
+  <section class="projects">
     <h1>Projetos</h1>
+
+    <form @submit.prevent="save">
+      <div class="field">
+        <label for="projectName" class="label"> Nome do projeto </label>
+        <input
+          type="text"
+          class="input"
+          v-model="projectName"
+          id="projectName"
+        />
+      </div>
+      <div class="field">
+        <button class="button" type="submit">Salvar</button>
+      </div>
+    </form>
+
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project in projects" :key="project.id">
+            <td>{{ project.id }}</td>
+            <td>{{ project.name }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import IProject from "@/interfaces/IProject";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: 'ProjectsView',
+  name: "ProjectsView",
+  data() {
+    return {
+      projectName: "",
+      projects: [] as IProject[],
+    };
+  },
+  methods: {
+    save(): void {
+      const project: IProject = {
+        name: this.projectName,
+        id: new Date().toISOString(),
+      };
 
+      this.projects.push(project);
+
+      this.projectName = "";
+    },
+  },
 });
-
 </script>
+
+<style lang="css" scoped>
+section h1 {
+  color: #000;
+}
+
+.projects {
+  padding: 1.25rem;
+}
+
+.label {
+  color: #000;
+}
+
+.table{
+    margin-top: 1rem;
+}
+</style>
