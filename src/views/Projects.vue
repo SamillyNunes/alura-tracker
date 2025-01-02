404eeb58e7d6e9e57ecfdbf895eed021d7180888
Samilly <samillynunes19@gmail.com>
2025-01-02 16:56:48 -0300

    chore: only presenting projects list in projects tab

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,83 +1,55 @@
 <template>
-  <section class="projects">
-    <h1>Projetos</h1>
-
-    <form @submit.prevent="save">
-      <div class="field">
-        <label for="projectName" class="label"> Nome do projeto </label>
-        <input
-          type="text"
-          class="input"
-          v-model="projectName"
-          id="projectName"
-        />
-      </div>
-      <div class="field">
-        <button class="button" type="submit">Salvar</button>
-      </div>
-    </form>
-
-    <table class="table is-fullwidth">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nome</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="project in projects" :key="project.id">
-          <td>{{ project.id }}</td>
-          <td>{{ project.name }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </section>
-</template>
-
-<script lang="ts">
-import { customUseStore } from "@/store";
-import { computed, defineComponent } from "vue";
-
-export default defineComponent({
-  name: "ProjectsView",
-  data() {
-    return {
-      projectName: "",
-    };
-  },
-  methods: {
-    save(): void {
-      // esse commit vai chamar a mutacao, e os params em seguida sao o que a mutacao recebe
-      this.store.commit('ADD_PROJECT', this.projectName);
-
-      this.projectName = "";
+    <section class="projects">
+      <h1>Projetos</h1>
+  
+      <table class="table is-fullwidth">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="project in projects" :key="project.id">
+            <td>{{ project.id }}</td>
+            <td>{{ project.name }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  </template>
+  
+  <script lang="ts">
+  import { customUseStore } from "@/store";
+  import { computed, defineComponent } from "vue";
+  
+  export default defineComponent({
+    name: "ProjectsView",
+    setup() {
+      const store = customUseStore();
+  
+      return {
+        projects: computed(()=> store.state.projects)
+      };
     },
-  },
-  setup() {
-    const store = customUseStore();
-
-    return {
-      store,
-      projects: computed(()=> store.state.projects)
-    };
-  },
-});
-</script>
-
-<style lang="css" scoped>
-section h1 {
-  color: #000;
-}
-
-.projects {
-  padding: 1.25rem;
-}
-
-.label {
-  color: #000;
-}
-
-.table {
-  margin-top: 1rem;
-}
-</style>
+  });
+  </script>
+  
+  <style lang="css" scoped>
+  section h1 {
+    color: #000;
+  }
+  
+  .projects {
+    padding: 1.25rem;
+  }
+  
+  .label {
+    color: #000;
+  }
+  
+  .table {
+    margin-top: 1rem;
+  }
+  </style>
+  
---
+++ src/views/Projects/ProjectForms.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="projects">
+    <h1>Projetos</h1>
+
+    <form @submit.prevent="save">
+      <div class="field">
+        <label for="projectName" class="label"> Nome do projeto </label>
+        <input
+          type="text"
+          class="input"
+          v-model="projectName"
+          id="projectName"
+        />
+      </div>
+      <div class="field">
+        <button class="button" type="submit">Salvar</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script lang="ts">
+import { customUseStore } from "@/store";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ProjectForms",
+  data() {
+    return {
+      projectName: "",
+    };
+  },
+  methods: {
+    save(): void {
+      // esse commit vai chamar a mutacao, e os params em seguida sao o que a mutacao recebe
+      this.store.commit('ADD_PROJECT', this.projectName);
+
+      this.projectName = "";
+    },
+  },
+  setup() {
+    const store = customUseStore();
+
+    return {
+      store,
+    };
+  },
+});
+</script>
+
+<style lang="css" scoped>
+section h1 {
+  color: #000;
+}
+
+.projects {
+  padding: 1.25rem;
+}
+
+.label {
+  color: #000;
+}
+
+</style>
