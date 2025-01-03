b75977f30c463e786374f3de50207f14a793280b
Samilly <samillynunes19@gmail.com>
2025-01-02 21:23:09 -0300

    chore: notifications component created

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,8 @@
     </div>
 
     <div class="column is-three-quarter content">
+      <Notifications/>
+
       <!-- Aqui vai as views de acordo com a rota -->
       <router-view> </router-view>
     </div>
@@ -17,10 +19,11 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import SideBar from "./components/SideBar.vue";
+import Notifications from "./components/Notifications.vue";
 
 export default defineComponent({
   name: "App",
-  components: { SideBar },
+  components: { SideBar, Notifications },
   data() {
     return {
       darkModeEnabled: false,
