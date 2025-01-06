bfa5ef2f992224cecebd7c84226053d93aa02679
Samilly <samillynunes19@gmail.com>
2025-01-06 12:17:01 -0300

    chore: rectoring project forms file to use composition api 2

--- src/views/Projects/ProjectForms.vue
+++ src/views/Projects/ProjectForms.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref,  } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 import { NotificationType } from "@/interfaces/INotification";
 import { customUseStore } from "@/store";
 import useNotifier from "@/hooks/notifier";
@@ -30,6 +30,7 @@ import {
   SEND_PROJECT_ACTION,
   UPDATE_PROJECT_ACTION,
 } from "@/store/actions_types";
+import { useRouter } from "vue-router";
 
 export default defineComponent({
   name: "ProjectForms",
@@ -38,59 +39,59 @@ export default defineComponent({
       type: String,
     },
   },
-  methods: {
-    save(): void {
-      if (this.id) {
-        this.store
-          .dispatch(UPDATE_PROJECT_ACTION, {
-            id: this.id,
-            name: this.projectName,
-          })
-          .then(() => this.onSavingSuccess());
-      } else {
-        // esse commit vai chamar a mutacao, e os params em seguida sao o que a mutacao recebe
-        this.store
-          .dispatch(SEND_PROJECT_ACTION, this.projectName)
-          .then(() => this.onSavingSuccess());
-      }
-    },
-    onSavingSuccess() {
-      this.projectName = "";
-
-      this.notify(
-        NotificationType.SUCCESS,
-        "Novo projeto foi salvo",
-        "Prontinho =D seu projeto está disponível"
-      );
-
-      this.$router.push("/projects");
-    },
-  },
   setup(props) {
+    const router = useRouter();
+
     const store = customUseStore();
     const { notify } = useNotifier();
 
     // esse ref() eh a criacao de uma variavel reativa
     const projectName = ref("");
 
-    
-    onMounted(()=>{
+    onMounted(() => {
       // Quando o componente for montado, se existir essa prop id, entao...
-      if (props.id ) {
+      if (props.id) {
         // aqui podemos acessar a this.store porque ela foi exportada la no setup()
         const project = store.state.project.projects.find(
           (p) => p.id === props.id
         );
-  
+
         projectName.value = project?.name || "";
       }
-      
     });
 
+    const save = () => {
+      if (props.id) {
+        store
+          .dispatch(UPDATE_PROJECT_ACTION, {
+            id: props.id,
+            name: projectName.value,
+          })
+          .then(() => onSavingSuccess());
+      } else {
+        // esse commit vai chamar a mutacao, e os params em seguida sao o que a mutacao recebe
+        store
+          .dispatch(SEND_PROJECT_ACTION, projectName.value)
+          .then(() => onSavingSuccess());
+      }
+    };
+
+    const onSavingSuccess = () => {
+      projectName.value = "";
+
+      notify(
+        NotificationType.SUCCESS,
+        "Novo projeto foi salvo",
+        "Prontinho =D seu projeto está disponível"
+      );
+
+      router.push("/projects");
+    };
+
     return {
-      store,
-      notify,
       projectName,
+      save,
+      onSavingSuccess,
     };
   },
 });
